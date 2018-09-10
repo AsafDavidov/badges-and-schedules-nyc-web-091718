9cf1d10f87e8a6363793e014de3d08116d93a35c
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 17:09:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 def assign_rooms(speakers)
   final_list = []
   rooms = Array (1..7)
-  rooms.each_index do |room| final_list.push("Hello, #{speakers[")
+  rooms.each_index do |room| final_list.push("Hello, #{speakers[room]}! You'll be assigned to room #{room}!")
 end
 def printer()
 end
