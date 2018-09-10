ecc03fb6450db16ec7246d05438140398efb1a19
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 17:05:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+  badge = "Hello, my name is #{name}."
+end
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name| badges.push(badge_maker(name))
+  end
+  badges
+end
+def assign_rooms(speakers)
+  rooms = Array (1..7)
+  
+end
+def printer()
+end
