6bec32f83df5b2009bf88d87abc19522b66b5611
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 17:17:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,5 +17,6 @@ def assign_rooms(speakers)
 end
 def printer(attendees)
   badges = batch_badge_creator(attendees)
+  rooms = assign_rooms(attendees)
   attendees.each_index do |attendee| puts 
 end
